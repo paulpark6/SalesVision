61cdbe46884f0481d4c8dbd4e63146a0be6d79fb
JEFF PARK <jeff@youngintl.net>
2025-09-20 03:13:14 +0000

    이 데시보드는 admission 데시보드에 적용

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -55,7 +55,7 @@ export default function DashboardPage() {
             <div className="grid gap-4 md:gap-8 lg:grid-cols-2 xl:grid-cols-3">
               <div className="grid auto-rows-max items-start gap-4 lg:col-span-2 lg:gap-8">
                 <SalesTargetChart />
-                <CumulativeSalesTargetChart />
+                <CumulativeSalesTargetChart isTeamData={true} />
                 <DuePaymentsTable />
               </div>
               <div className="grid auto-rows-max items-start gap-4 lg:gap-8">
