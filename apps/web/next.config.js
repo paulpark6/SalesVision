da8969b0baef81f2dc4a32e235356223ad1dfa1e
Paul Park <paulpark@mac.home>
2025-10-27 23:55:53 -0400

    data changes

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -2,6 +2,9 @@ const path = require('path');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    externalDir: true,
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
